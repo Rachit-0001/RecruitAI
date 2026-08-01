5cf6dbbe13c1d6a625c6a22b5f16fc2d4735ab25
Rachit Gangwar <156359363+Rachit-0001@users.noreply.github.com>
2026-08-02 04:33:38 +0530

    changes db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,15 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 4000,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: true,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
